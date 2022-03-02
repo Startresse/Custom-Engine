521f01ad2e2f3dcb32e39e1fdb3ba98caccf6e96
Startresse <startresse.guillard@gmail.com>
2022-03-02 14:59:21 +0100

    removed test_mesh

--- Custom Engine/Mesh.cpp
+++ Custom Engine/Mesh.cpp
@@ -79,6 +79,8 @@ Mesh::~Mesh()
         glDeleteVertexArrays(1, &vao);
     if (buffer)
         glDeleteBuffers(1, &buffer);
+    if (index_buffer)
+        glDeleteBuffers(1, &index_buffer);
 }
 
 Mesh read_mesh(std::string filename)
@@ -94,129 +96,4 @@ Mesh read_mesh(std::string filename)
     mesh.indices = { 0, 1, 2, 0, 2, 3 };
 
     return mesh;
-}
-
-
-test_mesh::~test_mesh()
-{
-    if (VAO)
-        glDeleteVertexArrays(1, &VAO);
-    if (VBO)
-        glDeleteBuffers(1, &VBO);
-    if (EBO)
-        glDeleteBuffers(1, &EBO);
-}
-
-test_mesh old_read_mesh(std::string filepath)
-{
-    test_mesh mesh;
-
-    std::ifstream file;
-
-    file.open(filepath);
-    if (!file.is_open()) {
-        std::cout << "Couldn't open file" << std::endl;
-        exit(0);
-    }
-
-    std::string line;
-
-    // Read vertices
-    while (std::getline(file, line))
-    {
-
-        if (line[0] == '#')
-            continue;
-
-        std::stringstream ss(line);
-        float x, y, z;
-        ss >> x >> y;
-        if (!(ss >> z))
-            break;
-
-        mesh.vertices.push_back(x);
-        mesh.vertices.push_back(y);
-        mesh.vertices.push_back(z);
-
-    }
-
-    // Read color
-    while (std::getline(file, line))
-    {
-
-        if (line[0] == '#')
-            continue;
-
-        std::stringstream ss(line);
-        float x, y, z;
-        ss >> x >> y;
-        if (!(ss >> z))
-            break;
-
-        mesh.vertices.push_back(x);
-        mesh.vertices.push_back(y);
-        mesh.vertices.push_back(z);
-
-    }
-
-    // Read indices
-    while (std::getline(file, line))
-    {
-
-        if (line[0] == '#')
-            continue;
-
-        int a, b, c;
-        std::stringstream ss(line);
-        ss >> a >> b;
-        if (!(ss >> c))
-            break;
-        mesh.indices.push_back(a);
-        mesh.indices.push_back(b);
-        mesh.indices.push_back(c);
-
-    }
-
-    file.close();
-
-    return mesh;
-}
-
-int test_mesh::generate_buffers()
-{
-    glGenBuffers(1, &EBO);
-    glGenBuffers(1, &VBO);
-    glGenVertexArrays(1, &VAO);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices[0]) * indices.size(), indices.data(), GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[0]) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
-
-    // position
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // color
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(sizeof(float) * vertices.size() / 2));
-    glEnableVertexAttribArray(1);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glBindVertexArray(0);
-
-    return 0;
-}
-
-void test_mesh::draw()
-{
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(nb_indices()), GL_UNSIGNED_INT, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
 }
--- Custom Engine/Mesh.h
+++ Custom Engine/Mesh.h
@@ -33,27 +33,3 @@ private:
 
 Mesh read_mesh(std::string filename);
 
-class test_mesh
-{
-public:
-
-    test_mesh() : VAO(0), VBO(0), EBO(0) {};
-
-    ~test_mesh();
-
-    int generate_buffers();
-
-    void draw();
-
-    unsigned int VAO, VBO, EBO;
-
-    size_t nb_vertices() { return indices.size(); }
-    size_t nb_indices() { return indices.size(); }
-    size_t nb_triangles() { return indices.size() / 3; }
-
-    std::vector<float> vertices;
-    std::vector<unsigned int> indices;
-
-};
-
-test_mesh old_read_mesh(std::string filepath);
